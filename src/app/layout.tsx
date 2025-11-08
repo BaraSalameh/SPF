4a95aa90b3469568bce1e309c3559d89e4ceea03
Bara E. Salameh <barasalameh90@gmail.com>
2025-11-08 17:51:22 +0300

    Updating metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
-    title: "Bara Salameh's Portfolio",
-    description: "Built to showcase my personal hard, and soft kills"
+    title: "Albaraa's Portfolio",
+    description: "Built to showcase my career"
 };
 
 const RootLayout = ({children}: Readonly<{children: React.ReactNode}>) => children
